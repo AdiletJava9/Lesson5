8a9a05aab7a1b875a064eda2abe6e7142bfe7f52
AdiletJava9 <adiibrahimov@gmail.com>
2025-01-19 01:35:18 +0600

    ispravlenie

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Boss boss = new Boss();
+
+
+        boss.setHealth(500);
+        boss.setDamage(50);
+        boss.setDefense("Physical");
+
+        System.out.println("Boss Health: " + boss.getHealth());
+        System.out.println("Boss Damage: " + boss.getDamage());
+        System.out.println("Boss Defense Type: " + boss.getDefense());
+        System.out.println("-------------------------------------");
+
+        Hero[] heroes = createHeroes();
+        for (Hero hero : heroes) {
+            System.out.println("Hero Health: " + hero.getHealth());
+            System.out.println("Hero Damage: " + hero.getDamage());
+            System.out.println("Hero Superpower: " + hero.getSuperPower());
+            System.out.println();
         }
     }
+
+    public static Hero[] createHeroes() {
+        Hero hero1 = new Hero(300, 50, "Invisibility");
+        Hero hero2 = new Hero(400, 60);
+        Hero hero3 = new Hero(250, 40, "Flight");
+
+        return new Hero[]{hero1, hero2, hero3};
+    }
 }
+
+
